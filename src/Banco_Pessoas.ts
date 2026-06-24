72a707cf96c769cd3a0b1f3f7bfa7080a71b2887
Nicolas <nicolas.david@unifesp.br>
2026-06-23 22:09:39 -0300

    Adcionando as classes necessárias, new , delete,

--- src/Banco_Pessoas.ts
+++ src/Banco_Pessoas.ts
@@ -2,6 +2,28 @@ import { Pessoa } from "./Pessoa";
 
 export class Banco_Pessoas {
     private pessoas: Pessoa[] = [];
-    private hashmap: Map<string, string> = new Map(); // Placeholder for the database connection
-    
+
+    adicionarPessoa(pessoa: Pessoa): void {//Aqui foi 
+        const jaExiste = this.pessoas.some((p) => p.id === pessoa.id);
+        if (!jaExiste) {
+            this.pessoas.push(pessoa);
+        }
+    }
+
+    removerPessoa(id: number): boolean {
+        const indice = this.pessoas.findIndex((p) => p.id === id);
+        if (indice !== -1) {
+            this.pessoas.splice(indice, 1);
+            return true;
+        }
+        return false;
+    }
+
+    buscarPessoa(id: number): Pessoa | undefined {
+        return this.pessoas.find((p) => p.id === id);
+    }
+
+    listarPessoas(): Pessoa[] {
+        return [...this.pessoas];
+    }
 }
